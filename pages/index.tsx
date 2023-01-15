d5ad9a78eb6c6b4b18b9a8977f5314726871dfbe
Tanaka Yuki <tnkyk0126@gmail.com>
2023-01-15 19:48:26 +0900

    second commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 
+import { Heading, Container } from '@chakra-ui/react'
 export default function Home() {
-  return <>大阪大学軽音楽部ROCK支援金プロジェクト</>
+  return (
+    <>
+      <Container maxW="container.lg" bg="#f3f3f3">
+        <Heading>大阪大学軽音楽部ROCK支援金プロジェクト</Heading>
+      </Container>
+    </>
+  )
 }
